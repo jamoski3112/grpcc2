16d604a17df8eb6c95a2ad936efc8b7ddf26d6ce
Rahul R <rahulraveendran06@gmail.com>
2024-11-19 22:07:13 +0400

    final

--- agent/standalone_agent.py
+++ agent/standalone_agent.py
@@ -30,17 +30,17 @@ class Agent:
             ('grpc.http2.max_pings_without_data', 0),
             ('grpc.http2.min_time_between_pings_ms', 10000),
             ('grpc.http2.min_ping_interval_without_data_ms', 5000),
+            ('grpc.ssl_target_name_override', 'merlin.rahulr.in'),
         ]
         
         # Create channel with SSL verification disabled
         credentials = grpc.ssl_channel_credentials()
-        channel_options = (('grpc.ssl_target_name_override', 'merlin.rahulr.in'),)
         
         # Create composite channel credentials
         self.channel = grpc.secure_channel(
             server_address,
             credentials,
-            options + channel_options
+            options
         )
         
         self.stub = agent_pb2_grpc.AgentServiceStub(self.channel)
